4e3628423b816930e7324888712f99e7f3b74bf0
Akshay Srivastava <akshaysrivastava896@gmail.com>
2026-06-24 00:40:42 +0530

    reverse elements

--- Arrays/reverseelements.java
+++ Arrays/reverseelements.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your size:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+       
 
+        System.out.println("enter your elements:");
+         for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+
+        }
+        System.out.println("reversed elements are");
+        for(int i=n-1;i>=0;i--){
+        
+            System.out.println(arr[i]+"  ");
+        }
+         sc.close();
+
+
+    }
+}
