84cd1ca54931cbccd134daa3e0567b2ae952d762
Akshay Srivastava <akshaysrivastava896@gmail.com>
2026-06-25 03:07:58 +0530

    Rename class to Main and improve search logic
    Refactor linear search implementation and rename class.

--- Arrays/linearsearch.java
+++ Arrays/linearsearch.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class linearsearch{
+public class Main{
     public static void main(String[]args){
      Scanner sc = new Scanner(System.in);
      System.out.println("Enter number of elemnts:");
@@ -9,22 +9,29 @@ public class linearsearch{
      for (int i=0;i<n;i++){
      arr[i]=sc.nextInt();
     }
-    boolean found = false;
+    boolean found =false;
     System.out.println("Enter your target element:");
     int target =sc.nextInt();
     for(int i=0;i<n;i++){
         if(arr[i]==target){
             found=true;
-        break;
-         }
-          
-         if(found){
-            System.out.println("milgya");
-         }
-         else{
-            System.out.println("nah mila..");
+            System.out.println("Found at index " + i);
             break;
-         }
+            }}
+            if(found==false){
+                 System.out.println("nah mila..");
+              
+            }
+            sc.close();
+
+            
+            
+           
         }
-    }
-}
+         
+         }
+        
+         
+        
+        
+        
